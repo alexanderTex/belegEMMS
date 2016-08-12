90f1153711005a02d6bef6b7190d96f36e7ac052
BeastyRiot <s0549906@htw-berlin.de>
2016-08-12 12:35:51 +0200

    double qualifier fixed

--- projectSogo/include/core/GameData.h
+++ projectSogo/include/core/GameData.h
@@ -135,7 +135,7 @@ public:
     static string Serialize(const GameData& data);
 
 
-    static GameData *GameData::Deserialize(std::string str) throw(DeserializationException);
+    static GameData *Deserialize(std::string str) throw(DeserializationException);
 
 
 private:
